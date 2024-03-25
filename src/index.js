e5a15ac16674251c385766287e55ad5658021873
Mustafa Hussain Zariwala <mustafazariwala5253@gmail.com>
2024-03-25 11:51:40 +1100

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// const moment = require('moment');
+// const _ = require('lodash');
+
+let textArea = document.getElementById('col-code-text');
+let resultArea = document.getElementById('col-result');
+
+// Set Default init value to textArea
+textArea.value = "function onInIt() {return [1,2,3,4,5]}\n\n\nonInIt()"
+resultArea.innerHTML = JSON.stringify(eval(textArea.value), null, 2)
+textArea.addEventListener('input', function(e) {
+  try {
+    let value = eval(e.target.value);
+    resultArea.innerHTML = JSON.stringify(value, null, 2)
+  } catch (error) {
+    resultArea.innerHTML = error.message;
+  }
+})

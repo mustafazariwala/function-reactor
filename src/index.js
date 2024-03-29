79ba78ef827f80113bbf2a007fa2dca6cf90daf7
Mustafa Hussain Zariwala <mustafazariwala5253@gmail.com>
2024-03-30 02:29:17 +1100

    More integrations and css fix

--- src/index.js
+++ src/index.js
@@ -15,11 +15,13 @@ let saveInput = document.getElementById('saveInput');
 let createNewButton = document.getElementById('col-button-createNew');
 
 const OnInIt = () => {
+  createAllButton();
   checkForCurrentFunction();
   prepareTextArea();
   prepareNewButton();
   prepareAllButton();
   prepareSaveButton();
+  prepareDeleteButton();
 }
 
 // checkForCurrentFunction checks if there is a current function in the local storage
@@ -39,7 +41,7 @@ function checkForCurrentFunction() {
 
 // prepareTextArea prepares the text area for input
 function prepareTextArea() {
-  textArea.addEventListener('input', function(e) {
+  textArea.addEventListener('keyup', function(e) {
     try {
       let value = editor.getSession().getValue()
       resultArea.innerHTML = JSON.stringify(eval(value), null, 2)
@@ -57,15 +59,16 @@ function prepareNewButton() {
     button.id = `func-${getLastNumberFromLocalStorage()}`
     button.innerText = 'New Function';
     document.getElementById('col-button').appendChild(button);
-    localStorage.setItem(button.id,  "function onInIt() {return [1,2,3,4,5]}\n\n\nonInIt()");
+    localStorage.setItem(button.id,  "// Included Script: lodash,moment \n\n function onInIt() {return ''}\n\n\nonInIt()");
     prepareButton(button)
+    clickButtonEvent(button.id, button)
   });
 }
 
 function getLastNumberFromLocalStorage() {
   let localStorageKeys = Object.keys(localStorage);
+  localStorageKeys = localStorageKeys.filter(key => key.match(/\w*-\d*$/));
   if(localStorageKeys && localStorageKeys.length > 0) {
-    localStorageKeys = localStorageKeys.filter(key => key.match(/\w*-\d*$/));
     localStorageKeys = localStorageKeys.map(key => parseInt(key.split('-')[1])).sort(function (a, b) {  return a - b;  })
     return localStorageKeys[localStorageKeys.length - 1] + 1
   }
@@ -81,22 +84,27 @@ function prepareAllButton() {
 
 function prepareButton(btn) {
   btn.addEventListener('click', (e) => {
-    localStorage.setItem('currentFunction', e.target.id);
-    editor.setValue(localStorage.getItem(e.target.id));
-    saveInput.value = localStorage.getItem(`${localStorage.getItem('currentFunction')}-name`) || '';
-    btn.classList.add('active');
-    for (let btn2 of functionButton) {
-      if (btn2 !== btn) {
-        btn2.classList.remove('active');
-      }
-    }
-    try {
-      let value = eval(localStorage.getItem(e.target.id));
-      resultArea.innerHTML = JSON.stringify(value, null, 2)
-    } catch (error) {
-      resultArea.innerHTML = error.message;
-    }
+    let id = e.target.id;
+    clickButtonEvent(id, btn)
   })
+}
+
+function clickButtonEvent(id, btn) {
+  localStorage.setItem('currentFunction', id);
+  editor.setValue(localStorage.getItem(id));
+  saveInput.value = localStorage.getItem(`${localStorage.getItem('currentFunction')}-name`) || '';
+  btn.classList.add('active');
+  for (let btn2 of functionButton) {
+    if (btn2 !== btn) {
+      btn2.classList.remove('active');
+    }
+  }
+  try {
+    let value = eval(localStorage.getItem(id));
+    resultArea.innerHTML = JSON.stringify(value, null, 2)
+  } catch (error) {
+    resultArea.innerHTML = error.message;
+  }
 }
 
 // prepareSaveButton prepares the save button
@@ -106,19 +114,32 @@ function prepareSaveButton() {
     let value = editor.getSession().getValue();
     localStorage.setItem(currentFunction, value)
     let input = saveInput.value;
-    localStorage.setItem(`${currentFunction}-name`, input)
-    document.getElementById(currentFunction).innerText = input;
+    if(input && input.length > 0 && input.trim().length > 0) {
+      localStorage.setItem(`${currentFunction}-name`, input)
+      document.getElementById(currentFunction).innerText = input;
+    }
   });
 }
 
-let localStorageKeys = Object.keys(localStorage);
-localStorageKeys = localStorageKeys.filter(key => key.match(/\w*-\d*$/));
-for (let key of localStorageKeys) {
-  let button = document.createElement('button');
-  button.classList.add('col-button-function');
-  button.id = key;
-  button.innerText = localStorage.getItem(`${key}-name`) || 'New Function';
-  document.getElementById('col-button').appendChild(button);
+function createAllButton() {
+  let localStorageKeys = Object.keys(localStorage);
+  localStorageKeys = localStorageKeys.filter(key => key.match(/\w*-\d*$/));
+  for (let key of localStorageKeys) {
+    let button = document.createElement('button');
+    button.classList.add('col-button-function');
+    button.id = key;
+    button.innerText = localStorage.getItem(`${key}-name`) || 'New Function';
+    document.getElementById('col-button').appendChild(button);
+  }
+}
+
+function prepareDeleteButton() {
+  deleteButton.addEventListener('click', (e) => {
+    let currentFunction = localStorage.getItem('currentFunction');
+    localStorage.removeItem(currentFunction);
+    localStorage.removeItem(`${currentFunction}-name`);
+    document.getElementById(currentFunction).remove();
+  });
 }
 
 OnInIt();

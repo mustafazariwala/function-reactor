76f9a6b7aa8c81b5c4f10ea9bc94984c92b1df3a
Mustafa Hussain Zariwala <mustafazariwala5253@gmail.com>
2024-04-04 22:24:41 +1100

    Added Instant Reload and 3rd Party Script

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-const moment = require('moment');
-const _ = require('lodash');
 
 // Editor Initialization
 var editor = ace.edit("col-code-text");
@@ -18,6 +16,13 @@ editor.commands.addCommand({
     saveButtonFunction()
   },
 });
+editor.commands.addCommand({
+  name: 'saveCommand',
+  bindKey: {win: "Ctrl-'",  mac: "Command-'"},
+  exec: async function(editor) {
+    await loadTextArea()
+  },
+});
 
 // Get all the elements
 let textArea = document.getElementById('col-code-text');
@@ -32,19 +37,27 @@ let resultTimeP = document.getElementById('col-result-time');
 let settingButton = document.getElementById("settingButton");
 let settingModal = document.getElementById("settingModal");
 let span = document.getElementById("settingModalClose");
+let displayTable = document.getElementById("display-table");
+let scriptSearchInput = document.getElementById("scriptSearchInput");
+let scriptSearchButton = document.getElementById("scriptSearchButton");
+let instantReloadCheckbox = document.getElementById("instant-reload-checkbox");
 
 let changesDone = false;
 
 const OnInIt = async () => {
-  createAllButton();
-  await checkForCurrentFunction();
-  prepareTextArea();
-  await prepareNewButton();
-  await prepareAllButton();
-  prepareSaveButton();
-  prepareDeleteButton();
-  prepareCopyButton();
-  setupModal();
+  await loadScriptFromLocalStorage();
+  window.addEventListener('load', async function() {
+    // your code here
+    createAllButton();
+    await checkForCurrentFunction();
+    prepareTextArea();
+    await prepareNewButton();
+    await prepareAllButton();
+    prepareSaveButton();
+    prepareDeleteButton();
+    prepareCopyButton();
+    await setupModal();
+  })
 }
 
 // checkForCurrentFunction checks if there is a current function in the local storage
@@ -63,17 +76,23 @@ async function checkForCurrentFunction() {
 }
 
 // prepareTextArea prepares the text area for input
-async function prepareTextArea() {
+async function prepareTextArea(ignoreCheckbox = false) {
   // textArea.addEventListener('keyup', function(e) {
   await editor.session.on('change', async function(e) {
-    ChangesDoneFunction(true);
+    if(instantReloadCheckbox.checked) {
+      await loadTextArea();
+    }
+  })
+}
+
+async function loadTextArea() {
+  ChangesDoneFunction(true);
     try {
       let value = editor.getSession().getValue()
       await setValue(value)
     } catch (error) {
       resultArea.innerHTML = error.message;
     }
-  })
 }
 // prepareNewButton prepares the new button
 async function prepareNewButton() {
@@ -83,7 +102,7 @@ async function prepareNewButton() {
     button.id = `func-${getLastNumberFromLocalStorage()}`
     button.innerText = 'New Function';
     document.getElementById('col-button').appendChild(button);
-    localStorage.setItem(button.id,  "// Included Script: lodash,moment \n\n function onInIt() {return ''}\n\n\nonInIt()");
+    localStorage.setItem(button.id,  "\n\nfunction onInIt() {\n  return '' \n}\n\n\nonInIt()");
     prepareButton(button)
     await clickButtonEvent(button.id, button)
   });
@@ -185,11 +204,16 @@ function prepareCopyButton() {
   });
 }
 
-async function setValue(RawValue, sec = moment().unix() ) {
-  resultArea.innerHTML = 'Loading...';
-  let value = await eval(RawValue);
-  resultArea.innerHTML = JSON.stringify(value, null, 2)
-  resultTimeP.innerText = `Took ${moment().unix() - sec}sec to execute`
+async function setValue(RawValue, sec = Date.now() ) {
+  try {
+    resultArea.innerHTML = 'Loading...';
+    let value = await eval(RawValue);
+    resultArea.innerHTML = JSON.stringify(value, null, 2)
+    resultTimeP.innerText = `Took ${Date.now() - sec}sec to execute`
+  // console.log(Date.now())
+  } catch (error) {
+    resultArea.innerHTML = error.message
+  }
 }
 
 function ChangesDoneFunction(changes = false) {
@@ -201,10 +225,10 @@ function ChangesDoneFunction(changes = false) {
   }
 }
 
-function setupModal() {
+async function setupModal() {
   settingButton.onclick = function() {
-    console.log('Hi')
     settingModal.style.display = "block";
+    searchScript()
   }
   span.onclick = function() {
     settingModal.style.display = "none";
@@ -214,6 +238,95 @@ function setupModal() {
       settingModal.style.display = "none";
     }
   }
+  scriptSearchButton.onclick = (e) => {
+    let value = scriptSearchInput.value;
+    if(value && value.length > 0) {
+      searchScript(value)
+    }
+  };
+  scriptSearchInput.addEventListener('keyup', (e) => {
+    if(e.keyCode === 13) {
+      console.log('Hit Enter')
+      let value = scriptSearchInput.value;
+      if(value && value.length > 0) {
+        searchScript(value)
+      }
+    }
+  })
 }
+
+async function searchScript(searchInput) {
+  displayTable.children[1].innerHTML = 'Loading...';
+  const params = new URLSearchParams({search: searchInput || '', limit: 30, fields: 'name,latest,description,version'});
+  let url = `https://api.cdnjs.com/libraries?${params}`;
+  let response = await fetch(url);
+  let libraries = await response.json();
+  let tableRow = document.createElement('tbody');
+  for(let lib of libraries.results) {
+    let tr = document.createElement('tr');
+    let name = document.createElement('td');
+    name.innerText = lib.name;
+    tr.appendChild(name);
+    let description = document.createElement('td');
+    description.innerText = lib.description;
+    description.style.textOverflow = 'ellipsis';
+    tr.appendChild(description);
+    let version = document.createElement('td');
+    version.innerText = lib.version;
+    tr.appendChild(version);
+    let link = document.createElement('td');
+    let addToHeader = document.createElement('button');
+    addToHeader.classList.add('button');
+    addToHeader.innerText = 'Add'
+    let scripts = localStorage.getItem('scripts');
+    scripts = scripts ? JSON.parse(scripts) : {};
+    if(scripts[lib.name]) {
+      addToHeader.innerText = 'Remove'
+      addToHeader.classList.add('delete');
+      // addToHeader.disabled = true;
+    }
+
+    addToHeader.addEventListener('click', async (e) => {
+      let scripts = localStorage.getItem('scripts');
+      scripts = scripts ? JSON.parse(scripts) : {};
+      if(!scripts[lib.name]) {
+        let script = document.createElement('script');
+        script.src = lib.latest;
+        script.id = lib.name;
+        scripts[lib.name] = lib.latest;
+        localStorage.setItem('scripts', JSON.stringify(scripts))
+        document.head.appendChild(script);
+        addToHeader.innerText = 'Remove'
+        addToHeader.classList.add('delete');
+      } else {
+        delete scripts[lib.name];
+        localStorage.setItem('scripts', JSON.stringify(scripts))
+        document.getElementById(lib.name).remove();
+        addToHeader.innerText = 'Add'
+        addToHeader.classList.remove('delete');
+      }
+    })
+    link.appendChild(addToHeader);
+    tr.appendChild(link);
+    tableRow.appendChild(tr);
+  }
+  displayTable.children[1].replaceWith(tableRow);
+}
+
+async function loadScriptFromLocalStorage() {
+  let scripts = localStorage.getItem('scripts');
+  if (scripts) {
+    scripts = JSON.parse(scripts);
+    for(let key in scripts) {
+      let script = document.createElement('script');
+      script.src = scripts[key];
+      script.id = key;
+      document.head.appendChild(script);
+    }
+  }
+}
+
+
+
 
 OnInIt();
